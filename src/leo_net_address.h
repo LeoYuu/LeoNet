c050cf562e1011db2ff97db6bd977f86dd344606
LeoYu <galaxy.leoyu@gmail.com>
2015-08-04 23:18:18 +0800

    1、添加对网络地址的封装。

---
+++ src/leo_net_address.h
@@ -0,0 +1,53 @@
+#ifndef __LEO_NET_ADDRESS_H__
+#define __LEO_NET_ADDRESS_H__
+
+#include <string>
+#include <sstream>
+
+#ifdef WIN32
+# include <winsock.h>
+#else
+# include <netinet/in.h>
+#endif
+
+class NetAddress
+{
+public:
+  NetAddress(std::string ip, unsigned short port)
+  {
+    memset(&addr_, 0, sizeof(addr_));
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    addr_.sin_addr.s_addr = inet_addr(ip.c_str());
+  }
+
+  NetAddress(const struct sockaddr_in& addr)
+    : addr_(addr)
+  {
+
+  }
+
+public:
+  unsigned short toPort() const
+  {
+    return ntohs(addr_.sin_port);
+  }
+
+  std::string toIp() const
+  {
+    return std::string(inet_ntoa(addr_.sin_addr));
+  }
+
+  std::string toIpPort() const
+  {
+    std::stringstream ss;
+    ss << toIp() << ":" << toPort();
+
+    return ss.str();
+  }
+  
+private:
+  struct sockaddr_in addr_;
+};
+
+#endif /* __LEO_NET_ADDRESS_H__ */
